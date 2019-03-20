e12076224b01a14a43d6629fc75eda7c016a27bd
AshburnLee <1578034415@qq.com>
2019-03-20 20:19:40 +0800

    modify

---
+++ _189RotateArray.cpp
@@ -0,0 +1,53 @@
+//
+// Created by junhui on 20/03/19.
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+
+    // time: O(n)
+    // space: O(n)
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%nums.size();
+
+        vector<int> tmp(nums.begin(), nums.end());
+        for (int i=n-1; i>= n-k; i--)
+            nums[k-(n-i)] = tmp[i];
+        for (int i=0; i<n-k; i++)
+            nums[k+i] = tmp[i];
+    }
+
+    //time: O(n)
+    //space: O(1)
+    void rotate2(vector<int>& nums, int k) {
+
+        k %= nums.size();
+        reverse(nums, 0, nums.size() - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.size() - 1);
+    }
+
+private:
+    void reverse(vector<int>& nums, int start, int end){
+        for(int i = start, j = end; i < j; i ++, j --)
+            swap(nums[i], nums[j]);
+    }
+};
+
+int main(int argc, char** argv){
+    vector<int> test = {1,2,3,4,5,6,7};
+
+    Solution().rotate(test, 8);
+
+    for(auto _: test)
+        cout<<_<<" ";
+    cout<<endl;
+
+    return 0;
+}
+
