63a0dadd8a9642f0f10370135af3f494e4939151
AshburnLee <1578034415@qq.com>
2019-03-28 11:07:06 +0800

    100

--- _100SameTree.cpp
+++ _100SameTree.cpp
@@ -14,54 +14,36 @@ struct TreeNode{
     TreeNode(int x):val(x), left(NULL), right(NULL) {}
 };
 
-// solve it by applying in order traverse;
-// dose not work when nodes have the same values!
-// need come up with a universally correct solution
 class Solution{
 private:
-    vector<int> resP;
-    vector<int> resQ;
-
-private:
-    bool equals(vector<int>& p, vector<int>& q){
-             if (p.size() != q.size())
-            return false;
-        for (auto itr1 = p.begin(),itr2=q.begin(); itr1!=p.end(), itr2!=q.end(); itr1++, itr2++)
-            if (*itr1 != *itr2){
-                return false;
-            }
-
-        return true;
-    }
+//    bool equals(vector<int>& p, vector<int>& q){
+//             if (p.size() != q.size())
+//            return false;
+//        for (auto itr1 = p.begin(),itr2=q.begin(); itr1!=p.end(), itr2!=q.end(); itr1++, itr2++)
+//            if (*itr1 != *itr2){
+//                return false;
+//            }
+//
+//        return true;
+//    }
 public:
+    // time: O(N). N is the number of nodes in the deeper tree
+    // space: O(M). M is the depth of the deeper tree
+    /*pass two tree nodes, and return true if two nodes have the same value, false otherwise*/
     bool isSameTree(TreeNode* p, TreeNode* q){
-
-        if (p == NULL && q == NULL)
+        if (!p && !q)
             return true;
 
-        inOrderP(p);
-        inOrderQ(q);
-
-        return equals(resP, resQ);
+        else if ( p && q && p->val==q->val )
+            return isSameTree( p->right, q->right ) && isSameTree( p->right, q->right );
+        else
+            return false;
     }
 
-    void inOrderP(TreeNode* p){
-        if (p != NULL ){
-            inOrderP(p->left);
-            resP.push_back(p->val);
-            inOrderP(p->right);
-        }
-    }
-    void inOrderQ(TreeNode* p){
-        if (p != NULL ){
-            inOrderQ(p->left);
-            resQ.push_back(p->val);
-            inOrderQ(p->right);
-        }
-    }
 };
 
 int main(int argc, char** argv){
+
     TreeNode* root1 = new TreeNode(1);
     TreeNode* left1 = new TreeNode(2);
     root1->left = left1;
@@ -69,22 +51,14 @@ int main(int argc, char** argv){
     root1->right = right1;
 
     TreeNode* root2 = new TreeNode(1);
-//    TreeNode* left2 = new TreeNode(2);
-//    root2->left = left2;
-    TreeNode* right2 = new TreeNode(2);
+    TreeNode* left2 = new TreeNode(2);
+    root2->left = left2;
+    TreeNode* right2 = new TreeNode(3);
     root2->right = right2;
 
 
     Solution sol;
-    sol.inOrderP(root1);
-    sol.inOrderQ(root2);
-//    sol.printResP();
-//    sol.printResQ();
     cout<<sol.isSameTree(root1, root2)<<endl;
 
-//    vector<int> a = {2,1,3};
-//    vector<int> b = {2,1};
-//
-//    cout<<sol.equals(a,b)<<endl;
     return 0;
 }
