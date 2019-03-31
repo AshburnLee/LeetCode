105cc5d2e7064c5ec029ddd81590b1d2e53bb313
AshburnLee <1578034415@qq.com>
2019-03-31 14:17:07 +0800

    263

--- _263UglyNumber.cpp
+++ _263UglyNumber.cpp
@@ -7,14 +7,15 @@ using namespace std;
 class Solution{
 public:
     bool isUgly(int num) {
-
+        if (num<=0 || num==1) return false;
         while(num>0){
             if (num%2==0) num/=2;
             else if(num%3==0) num/=3;
             else if (num%5==0) num/=5;
             else return num == 1;
         }
-        if (num<=0 || num==1) return false;
+        throw invalid_argument("No solution");
+
     }
 
     // optimize the above
