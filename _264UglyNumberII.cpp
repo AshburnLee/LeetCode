697430613fd9b45dc0fc09e258ddcfe6ec6de519
AshburnLee <1578034415@qq.com>
2019-03-31 17:01:00 +0800

    264 need a better algorithm

--- _264UglyNumberII.cpp
+++ _264UglyNumberII.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cassert>
 #include <climits>
+#include <vector>
 
 using namespace std;
 
@@ -34,10 +35,37 @@ public:
         return num==1;
     }
 
-    
+public:
+    int nthUglyNumber2(int n) {
+        vector <int> results (1,1);
+        int i = 0, j = 0, k = 0;
+        while (results.size() < n)
+        {
+            results.push_back(min(results[i] * 2, min(results[j] * 3, results[k] * 5)));
+            if (results.back() == results[i] * 2) ++i;
+            if (results.back() == results[j] * 3) ++j;
+            if (results.back() == results[k] * 5) ++k;
+        }
+        return results.back();
+    }
+
+public:
+    int nthUglyNumber3(int n) {
+        vector<int> ugly(n, 1);
+        int c2 = 2, c3 = 3, c5 = 5;
+        int i2 = 0, i3 = 0, i5 = 0;
+        for (int i=1; i<n; ++i) {
+            int last = ugly[i] = min(c2, min(c3, c5));
+            while (c2 <= last) c2 = 2 * ugly[++i2];
+            while (c3 <= last) c3 = 3 * ugly[++i3];
+            while (c5 <= last) c5 = 5 * ugly[++i5];
+        }
+        return ugly[n-1];
+    }
 };
 
+
 int main(int argc, char** argv){
-    cout<<Solution().nthUglyNumber(1352)<<endl;
+    cout<<Solution().nthUglyNumber3(1352)<<endl;
     return 0;
 }
