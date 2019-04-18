737f60763a5d9e70a049edf3ef8dded5a08d4c9f
AshburnLee <1578034415@qq.com>
2019-04-18 21:41:58 +0800

    905

--- _905SortArrayByParity.cpp
+++ _905SortArrayByParity.cpp
@@ -21,11 +21,42 @@ public:
         }
         return tmp;
     }
+
+    // in-space
+    // time: O(N)   98.30%
+    // space: O(1)  99.68%
+    void sortArrayByParity2(vector<int>& A){
+        int odd = 0;
+        int even = A.size()-1;
+        while(odd < even){
+            if      (A[odd]%2==1 && A[even]%2==0)
+                swap(A[odd++], A[even--]);
+            else if (A[odd]%2==0 && A[even]%2==1) {
+                odd++;
+                even--;
+            }
+            else if (A[odd]%2==1 && A[even]%2==1)
+                even--;
+            else if (A[odd]%2==0 && A[even]%2==0)
+                odd++;
+        }
+        return;
+    }
 };
 
 int main(int argc, char** argv){
+    Solution sol;
     vector<int> tst = {1,3,5,6,7,8,9};
-    vector<int> res = Solution().sortArrayByParity(tst);
 
+    vector<int> res = sol.sortArrayByParity(tst);
+    sol.sortArrayByParity2(tst);
+
+    for (auto& _: res)
+        cout<<_<<" ";
+    cout<<endl;
+
+    for (auto& _: tst)
+        cout<<_<<" ";
+    cout<<endl;
     return 0;
 }
