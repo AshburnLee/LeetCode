d95033723577f870baf58d7e4414426b179c4283
AshburnLee <1578034415@qq.com>
2019-05-07 20:44:49 +0800

    200

--- _200NumberofIslands.cpp
+++ _200NumberofIslands.cpp
@@ -3,60 +3,68 @@
 //
 #include <iostream>
 #include <vector>
+#include <cassert>
 using namespace std;
 
-class Solution {
+//time:  O(M*N)   98.77%
+//space: O(M*N)   82.98%
+class Solution{
 private:
-    int d[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
-    int m,n;
+    int move[4][2] = {{-1,0},{0,1},{1,0},{0,-1}}; // left, up, right, down
+    int m, n;
     vector<vector<bool>> visited;
 
-    bool inArea(int x, int y){
-        return x>=0 && x<m && y>=0 && y<n;
+    bool validArea(int x, int y){
+        return x>=0 && x<n && y>=0 && y<m;
     }
 
     void dfs(vector<vector<char>>& grid, int x, int y){
         visited[x][y] = true;
-
         for (int i=0; i<4; i++){
-            // update x,y
-            int newx = x + d[i][0];
-            int newy = y + d[i][1];
+            int newx = x + move[i][0];
+            int newy = y + move[i][1];
 
-            if (inArea(newx,newy) && !visited[newx][newy] && grid[newx][newy]=='1'){
+            // for all constrains, big scale constrains first, the small constrains !!!
+            if (validArea(newx, newy) && !visited[newx][newy] && grid[newx][newy] == '1'){
                 dfs(grid, newx, newy);
             }
         }
+        return;
     }
 
 public:
-    // time:    98.78%
-    // space:    95.29%
     int numIslands(vector<vector<char>>& grid) {
-        m = grid.size();
-        if (m == 0)
-            return 0;
-        n = grid[0].size();
-        if (n == 0)
-            return 0;
 
-        visited = vector<vector<bool>> (m, vector<bool>(n, false));
+        if (grid.empty()) return 0;
+        m = grid[0].size();
+        n = grid.size();
+        if ( m==0 || n==0) return 0;
+        assert(m>0 && n>0);
+
+        for (int i=0; i<n; i++)
+            visited.push_back(vector<bool>(m, false));
 
         int res = 0;
-        // traverse every points
-        for (int i=0; i<m; i++){
-            for (int j=0; j<n; j++){
-                if (grid[i][j] == '1' && !visited[i][j]){
+        for (int i=0; i<n; i++)
+            for (int j=0; j<m; j++)
+                if (!visited[i][j] && grid[i][j] == '1' ){
+                    dfs(grid, i ,j);
                     res++;
-                    dfs(grid, i, j);
                 }
-            }
-        }
+
         return res;
     }
 };
 
 int main(int argc, char** argv){
+    vector<vector<char>> grid = {{'1','1','0','0','0'},
+                                 {'1','1','0','0','0'},
+                                 {'0','0','1','0','0'},
+                                 {'0','0','0','1','1'}};
+    cout<<Solution().numIslands(grid)<<endl;
+
+    vector<vector<char>> grid2 = {};
+    cout<<Solution().numIslands(grid2)<<endl;
 
 
 }
