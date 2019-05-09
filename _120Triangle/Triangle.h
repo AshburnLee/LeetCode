5ed222a652f56de0d3196e4a415da91b5cbe16cb
AshburnLee <1578034415@qq.com>
2019-05-09 20:38:49 +0800

    120

--- _120Triangle/Triangle.h
+++ _120Triangle/Triangle.h
@@ -5,18 +5,20 @@
 #ifndef _120TRIANGLE_TRIANGLE_H
 #define _120TRIANGLE_TRIANGLE_H
 
+/// top-down DP
+/// Time: O(n^2)   99.97%
+/// Space: O(1)    93.85%
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-/// Time Complexity: O(n^2)
-/// Space Complexity: O(1)
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
+
+        triangle[0][0] = triangle[0][0];
 
         for (int i = 1; i<n; i++){
             triangle[i][0] += triangle[i-1][0];
@@ -25,7 +27,7 @@ public:
             for (int j=1;j<i;j++)
                 triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j]);
         }
-//        cout<<triangle[]
+
         return *min_element(triangle[n-1].begin(), triangle[n-1].end());
 
     }
