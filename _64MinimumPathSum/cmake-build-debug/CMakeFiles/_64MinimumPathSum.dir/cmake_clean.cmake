cf2f3108667d81f1411b6c7fd89a2063214e5099
AshburnLee <1578034415@qq.com>
2019-05-09 23:56:37 +0800

    64

---
+++ _64MinimumPathSum/cmake-build-debug/CMakeFiles/_64MinimumPathSum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_64MinimumPathSum.dir/main.cpp.o"
+  "_64MinimumPathSum.pdb"
+  "_64MinimumPathSum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_64MinimumPathSum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
