b11e8ced75e4876fd16aa7e9197e44f0bfc198a1
AshburnLee <1578034415@qq.com>
2019-05-09 20:38:22 +0800

    120

--- _120Triangle/main.cpp
+++ _120Triangle/main.cpp
@@ -4,14 +4,20 @@
 
 #include <iostream>
 #include "Triangle.h"
+#include "Triangle2.h"
 
 using namespace std;
 int main() {
-    vector<vector<int>> triangle = { {4},
-                                     {5,6},
-                                     {4,5,3},
-                                     {4,3,1,2}};
-
+    vector<vector<int>> triangle = { {2},
+                                     {3,4},
+                                     {6,5,7},
+                                     {4,1,8,3}};
     cout<<Solution().minimumTotal(triangle)<<endl;
+
+    vector<vector<int>> triangle2 = { {2},
+                                      {3,4},
+                                      {6,5,7},
+                                      {4,1,8,3}};
+    cout<<Solution2().minimumTotal(triangle2)<<endl;
     return 0;
 }
