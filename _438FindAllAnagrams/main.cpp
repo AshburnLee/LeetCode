22ce6e02a1387ae7107a54d97d09306462d1bb22
AshburnLee <1578034415@qq.com>
2019-05-19 00:03:44 +0800

    438

--- _438FindAllAnagrams/main.cpp
+++ _438FindAllAnagrams/main.cpp
@@ -3,32 +3,13 @@
 using namespace std;
 
 int main() {
-//    string s = "dcbaebabacd";
-//    string p = "abc";
-//    vector<int> res = Solution().findAnagrams(s, p);
-//
-//    for (auto& i: res)
-//        cout<<i<<endl;
-//    cout<<endl;
+    string s = "abcddddabcdddabc";
+    string p = "bca";
 
-    string s = "bca";
-    unordered_map<char, int> mp;
-    mp['a'] = 1;
-    mp['b'] = 1;
-    mp['c'] = 1;
-
-    mp['a'] --;
-    cout<<(mp['a']==0)<<endl;
-    if (mp['a']==0)
-        mp.erase(mp['a']);
-
-    for (unordered_map<char, int>::iterator itr = mp.begin(); itr!=mp.end(); itr++){
-        cout<<(*itr).first<<" "<<(*itr).second<<endl;
-    }
-
-    cout<<(mp.find('a') != mp.end())<<endl;
-
-
+    vector<int> res = Solution().findAnagrams(s, p);
+    for (auto& i: res)
+        cout<<i<<endl;
+    cout<<endl;
 
     return 0;
 }
