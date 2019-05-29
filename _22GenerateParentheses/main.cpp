2cc98a60874d4ba4be9c25698b150027d3c1205e
AshburnLee <1578034415@qq.com>
2019-05-29 22:49:21 +0800

    22

--- _22GenerateParentheses/main.cpp
+++ _22GenerateParentheses/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include "Solution.h"
+#include "Solution2.h"
 
 using namespace std;
 
 int main() {
-    vector<string> res = Solution().generateParenthesis(5);
-
+    vector<string> res = Solution2().generateParenthesis(3);
     for (auto& i: res)
+        cout<<i<<endl;
+
+    cout<<endl;
+
+    vector<string> res2 = Solution().generateParenthesis(3);
+    for (auto& i: res2)
         cout<<i<<endl;
     return 0;
 }
