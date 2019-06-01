3bd6df0ee663084d2272e79f458da2a973330b01
AshburnLee <AshburnLee@github.com>
2019-06-01 17:16:59 +0800

    73

---
+++ _73SetMatrixZeros/Solution.h
@@ -0,0 +1,39 @@
+
+#ifndef _73SETMATRIXZEROS_SOLUTION
+#define _73SETMATRIXZEROS_SOLUTION
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* O(M+N) and constant space solutions */
+/// time: O(N*M)   89.45%
+/// space: O(1)   6.62%
+class Solution{
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        // find positions of '0's
+        vector<pair<int,int>> zeros;
+        for (int r=0; r<row; r++){
+            for (int c=0; c<col; c++){
+                if (matrix[r][c] == 0)
+                    zeros.push_back(make_pair(r,c));
+            }
+        }
+
+        // operation
+        for (auto p: zeros){
+            for (int c=0; c<col; c++)
+                matrix[p.first][c] = 0;
+            for (int r=0; r<row; r++)
+                matrix[r][p.second] = 0;
+        }
+
+    }
+};
+
+#endif
