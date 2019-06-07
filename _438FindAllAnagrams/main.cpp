7d8a1240b9e806b1cf0295c660a31993def2e11d
AshburnLee <1578034415@qq.com>
2019-06-07 18:30:25 +0800

    main.cpp

--- _438FindAllAnagrams/main.cpp
+++ _438FindAllAnagrams/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include "FindAllAnagrams.h"
+#include "FindAllAnagrams2.h"
 using namespace std;
 
+
 int main() {
-    string s = "abcddddabcdddabc";
-    string p = "bca";
+    string s ="abbbabbabba";
+    string p = "abb";
 
     vector<int> res = Solution().findAnagrams(s, p);
     for (auto& i: res)
-        cout<<i<<endl;
+        cout<<i<<" ";
     cout<<endl;
+
+    vector<int> res2 = Solution2().findAnagrams(s, p);
+    for (auto& i: res2)
+        cout<<i<<" ";
+    cout<<endl;
+
 
     return 0;
 }
+
