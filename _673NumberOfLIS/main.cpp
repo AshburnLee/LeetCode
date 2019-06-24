1153c48dad2dde9fefc2b1eb438eab251ccc81f2
AshburnLee <1578034415@qq.com>
2019-06-24 15:58:16 +0800

    673

---
+++ _673NumberOfLIS/main.cpp
@@ -0,0 +1,7 @@
+#include "Solution.h"
+
+int main(int argc, char** argv){
+    vector<int> nums={10, 9, 2,5,3,7,101, 18, 20, 19};
+    cout<<Solution().findNumberOfLIS(nums)<<endl;
+    return 0;
+}
