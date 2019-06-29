cc18ff9b8b8d3e4712cd78ede3304576981736ff
AshburnLee <1578034415@qq.com>
2019-06-29 21:35:19 +0800

    1089

--- _1089DuplicateZeros/main.cpp
+++ _1089DuplicateZeros/main.cpp
@@ -1,11 +1,21 @@
 #include "Solution.h"
+#include "Solution2.h"
 
 int main(int argc, char** argv){
+	
 	vector<int> arr = {1,0,2,3,0,4,5};
 	Solution().duplicateZeros(arr);
 
 	for (auto i:arr)
 		cout<<i<<" ";
 	cout<<endl;
+
+	vector<int> arr2 = {1,0,2,3,0,4,5};
+	Solution2().duplicateZeros(arr2);
+
+	for (auto i:arr2)
+		cout<<i<<" ";
+	cout<<endl;
+
 	return 0;
 }
