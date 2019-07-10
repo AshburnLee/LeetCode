cc448a23155178d5b00eb85692d16e1d85d77c9a
AshburnLee <1578034415@qq.com>
2019-07-10 23:44:41 +0800

    435 main.cpp

--- _435NonOverlappingIntervals/main.cpp
+++ _435NonOverlappingIntervals/main.cpp
@@ -1,10 +1,11 @@
 #include "Solution.h"
+#include "Solution2.h"
 
 int main(int argc, char** argv){
 	Interval* inter1 = new Interval(1,2);	
-	Interval* inter2 = new Interval(4,5);	
-	Interval* inter3 = new Interval(6,7);	
-	Interval* inter4 = new Interval(7,8);	
+	Interval* inter2 = new Interval(2,3);	
+	Interval* inter3 = new Interval(1,3);	
+	Interval* inter4 = new Interval(3,4);	
 
 	vector<Interval*> input;
 
@@ -18,6 +19,12 @@ int main(int argc, char** argv){
 	}
 	cout<<endl;
 
+	cout<<"Solution: \t";
 	cout<<Solution().eraseOverLapIntervals(input)<<endl;
+
+	vector<vector<int>> intervals = {{1,2},{2,3},{1,3},{3,4}};
+
+	cout<<"SOlution2: \t";
+	cout<<Solution2().eraseOverLapIntervals(intervals)<<endl;
 	return 0;
 }
