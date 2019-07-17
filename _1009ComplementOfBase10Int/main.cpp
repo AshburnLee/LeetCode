6e8b009776cf0f4d26f08a99547ceee11c0cee73
AshburnLee <1578034415@qq.com>
2019-07-17 17:54:41 +0800

    1009 main.cpp

---
+++ _1009ComplementOfBase10Int/main.cpp
@@ -0,0 +1,9 @@
+#include "Solution.h"
+
+int main(int argc, char** argv){
+
+	cout<<Solution().bitwiseComplement(5)<<endl;
+	cout<<Solution().bitwiseComplement(7)<<endl;
+	cout<<Solution().bitwiseComplement(10)<<endl;
+	return 0;
+}
