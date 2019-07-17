a7ed0cbb38255c05093e1a9037467077455425ce
AshburnLee <1578034415@qq.com>
2019-07-18 00:18:37 +0800

    1122 main.cpp

---
+++ _1122RelativeSortArray/main.cpp
@@ -0,0 +1,11 @@
+#include "Solution.h"
+
+int main(int argc, char** argv){
+	vector<int> arr1 = {2,3,1,3,2,4,6,7,9,2,19};
+	vector<int> arr2 = {2,1,4,3,9,6};
+
+	vector<int> res = Solution().relativeSortArray(arr1, arr2);
+
+	
+	return 0;
+}
