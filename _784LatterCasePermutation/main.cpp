5d9f9928172aa8394803a860489ee53af6c3c851
AshburnLee <1578034415@qq.com>
2019-07-25 00:58:23 +0800

    784 main

--- _784LatterCasePermutation/main.cpp
+++ _784LatterCasePermutation/main.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main(int argc, char** argv){
 
+	vector<string> res;
+	string s = "s3g5";
+	res = Solution().letterCasePermutation(s);
+	for (auto i:res)
+		cout<<i<<" ";
+	cout<<endl;
+
+
 	return 0;
 }
