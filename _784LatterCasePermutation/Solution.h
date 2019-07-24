aa8345a49c1c46b4aad9448dfd4ae7c95bace42c
AshburnLee <1578034415@qq.com>
2019-07-25 00:58:51 +0800

    784 solution

---
+++ _784LatterCasePermutation/Solution.h
@@ -0,0 +1,37 @@
+#ifndef _784LETTERCASEPERMUTATION_SOLUTION_H
+#define _784LETTERCASEPERMUTATION_SOLUTION_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+/// time: O(N)
+///space: O(N)
+class Solution{
+	public:
+		std::vector<std::string> letterCasePermutation(std::string S){
+			std::vector<std::string> res;
+			backTrack(S, 0, res);
+			return res;
+		}
+
+	private:
+		void backTrack(std::string& s, int i, std::vector<std::string>& res){
+			if (i==s.size()){
+				res.push_back(s);
+				return;
+			}
+
+			backTrack(s, i+1, res);
+            
+            	char c = s[i];
+           	if (isalpha(c)){
+				s[i] = islower(c) ? toupper(c) : tolower(c);
+				backTrack(s, i+1, res);
+			}
+		
+		}
+
+};
+
+#endif
